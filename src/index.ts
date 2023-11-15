b62d580eb79636736ae92ec0e5089e163e66070f
Anna Roses <anna.roses.marti@gmail.com>
2023-11-15 23:45:14 +0100

    Add call to connectToDatabase to startServer

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import express from "express";
 import morgan from "morgan";
-import { app } from "./server/app.js";
+import chalk from "chalk";
+import { app, startServer } from "./server/app.js";
 import pingRouter from "./features/ping/router/pingRouter.js";
+import { connectToDatabase } from "./dataBase/index.js";
 
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/", pingRouter);
+
+if (!process.env.MONGODB_URL) {
+  console.log(chalk.red(chalk.red("Missing MongoDB Connection String")));
+  process.exit();
+}
+
+const mongoUrl = process.env.MONGODB_URL;
+const port = process.env.PORT ?? 4000;
+
+await connectToDatabase(mongoUrl);
+startServer(+port);
