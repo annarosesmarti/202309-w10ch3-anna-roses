5908f61118640b47fe6d0c5cdef87eaeab273e54
Anna Roses <anna.roses.marti@gmail.com>
2023-11-15 23:23:36 +0100

    Add pingRouter and app.listen

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+import express from "express";
 import morgan from "morgan";
-import app from "./app.js";
+import { app } from "./server/app.js";
+import pingRouter from "./features/ping/router/pingRouter.js";
 
 app.use(morgan("dev"));
+app.use(express.json());
+app.use("/", pingRouter);
