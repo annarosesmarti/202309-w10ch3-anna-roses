09d126020dcd293bb86e946c61e85458670152ae
Anna Roses <anna.roses.marti@gmail.com>
2023-11-16 23:49:28 +0100

    Add zipiZapeSchema

---
+++ src/features/zipiZape/model/zipiZape.ts
@@ -0,0 +1,31 @@
+import { Schema, model } from "mongoose";
+import { type ZipiZapeStructure } from "../types.js";
+
+const zipiZapeSchema = new Schema<ZipiZapeStructure>({
+  name: {
+    type: String,
+    required: true,
+  },
+  characteristic: {
+    type: String,
+    required: true,
+    enum: ["Mischievous Twin"],
+  },
+  hair: {
+    type: String,
+    required: true,
+    enum: ["blond", "black"],
+  },
+  complement: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const ZipiZape = model("ZipiZape", zipiZapeSchema, "zipizape");
+
+export default ZipiZape;
