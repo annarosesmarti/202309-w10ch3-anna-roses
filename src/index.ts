cdb17b7efb45ac363aa6716a37ced817095e0a70
Anna Roses <anna.roses.marti@gmail.com>
2023-11-17 00:23:04 +0100

    Move middlewares to server index

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,16 @@
-import express from "express";
-import morgan from "morgan";
 import chalk from "chalk";
-import { app, startServer } from "./server/app.js";
-import pingRouter from "./features/ping/router/pingRouter.js";
-import { connectToDatabase } from "./dataBase/index.js";
+import { startServer } from "./server/app.js";
+import "./server/index.js";
+import connectToDatabase from "./database/index.js";
 
-app.use(morgan("dev"));
-app.use(express.json());
-app.use("/", pingRouter);
+const port = process.env.PORT ?? 4000;
 
 if (!process.env.MONGODB_URL) {
-  console.log(chalk.red(chalk.red("Missing MongoDB Connection String")));
+  console.log(chalk.red("Missing MongoDB Connection String"));
   process.exit();
 }
 
 const mongoUrl = process.env.MONGODB_URL;
-const port = process.env.PORT ?? 4000;
 
 await connectToDatabase(mongoUrl);
 startServer(+port);
---
+++ src/server/index.ts
@@ -0,0 +1,13 @@
+import morgan from "morgan";
+import express from "express";
+import { app } from "./app.js";
+import pingRouter from "../features/ping/router/pingRouter.js";
+import zipiZapeRouter from "../features/zipiZape/router/zipiZapeRouter.js";
+
+app.use(morgan("dev"));
+
+app.use(express.json());
+
+app.use("/", pingRouter);
+
+app.use("/zipizape", zipiZapeRouter);
