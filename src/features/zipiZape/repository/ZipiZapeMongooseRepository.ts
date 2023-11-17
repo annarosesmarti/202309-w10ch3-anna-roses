05101043b40bd3a1355fe113b3f315ec00d996c5
Anna Roses <anna.roses.marti@gmail.com>
2023-11-17 16:09:00 +0100

    Add getZipiZapeById in Repository

--- src/features/zipiZape/repository/ZipiZapeMongooseRepository.ts
+++ src/features/zipiZape/repository/ZipiZapeMongooseRepository.ts
@@ -7,6 +7,18 @@ class ZipiZapeMongooseRepository implements ZipiZapeRepository {
 
     return zipiZape;
   };
+
+  public getZipiZapeById = async (
+    twinId: string,
+  ): Promise<ZipiZapeStructure> => {
+    const zipiZape = await ZipiZape.findById(twinId);
+
+    if (!zipiZape) {
+      throw new Error("Id not found");
+    }
+
+    return zipiZape;
+  };
 }
 
 export default ZipiZapeMongooseRepository;
