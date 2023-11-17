fd3b1850d05c81410f0dabd275d042e40200933a
Anna Roses <anna.roses.marti@gmail.com>
2023-11-17 16:13:56 +0100

    Refactor index server enters

--- src/server/index.ts
+++ src/server/index.ts
@@ -5,9 +5,7 @@ import pingRouter from "../features/ping/router/pingRouter.js";
 import zipiZapeRouter from "../features/zipiZape/router/zipiZapeRouter.js";
 
 app.use(morgan("dev"));
-
 app.use(express.json());
 
 app.use("/", pingRouter);
-
 app.use("/zipizape", zipiZapeRouter);
